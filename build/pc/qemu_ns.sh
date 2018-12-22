61b991b40cfcc3e0e5a09087e4cea5763f73e20c
Diego <diego.gallizioli@libero.it>
2018-12-22 18:33:01 +0100

    build/pc: add qemu_ns.sh script

---
+++ build/pc/qemu_ns.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+sudo qemu-system-i386 -cpu pentium2 -m 16 -netdev tap,id=tap0 -device rtl8139,netdev=tap0 -vga std -fda bootflop.img -boot a -serial telnet:127.0.0.1:10000,server,nowait -monitor stdio
