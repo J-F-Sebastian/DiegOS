0a986413a065f7c474fe7b8f838fe991e32288b8
Diego <diego.gallizioli@libero.it>
2022-02-20 22:22:50 +0100

    run-indent.sh: add single file mode

--- run-indent.sh
+++ run-indent.sh
@@ -1,5 +1,11 @@
 #!/bin/sh
-echo "Indent all codebase ... "
-find . -name *.[c,h] -execdir indent -linux -i8 -l100 {} \;
-find . -name *.[c,h]~ -delete
+if [ $1 == "" ]; then
+	echo "Indent all codebase ... "
+	find . -name *.[c,h] -execdir indent -linux -i8 -l100 {} \;
+	find . -name *.[c,h]~ -delete
+else
+	echo "Indent ${1} ... "
+	indent -linux -i8 -l100 $1 -o $1
+fi
 echo "Done."
+
