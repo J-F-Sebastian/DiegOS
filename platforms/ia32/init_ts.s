31302f4ac0a71262255e2ef316e597fd071a5f9c
Diego <diego.gallizioli@libero.it>
2022-03-19 19:19:23 +0100

    platforms/ia32/init_ts.s: add set_ts

---
+++ platforms/ia32/init_ts.s
@@ -0,0 +1,32 @@
+/*
+ * DiegOS Operating System source code
+ *
+ * Copyright (C) 2012 - 2015 Diego Gallizioli
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+.text
+.globl set_ts
+.type  set_ts, @function
+
+/*
+ * void set_ts ();
+ */
+set_ts:
+movl	%cr0, %eax
+/* Bit 3 TS - Task Switched enables exceptions on executing FP/MMX/SSE instructions */
+orl     $0x8, %eax
+movl    %eax, %cr0
+ret
