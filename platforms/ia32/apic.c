f5ff156a1fc71fe76bcf3f2c814f63ec4495e7ea
Diego <diego.gallizioli@libero.it>
2022-04-02 19:28:05 +0200

    platforms/ia32: add APIC support

---
+++ platforms/ia32/apic.c
@@ -0,0 +1,106 @@
+/*
+ * DiegOS Operating System source code
+ *
+ * Copyright (C) 2012 - 2022 Diego Gallizioli
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#include <string.h>
+#include <stdio.h>
+#include <types_common.h>
+#include "apic.h"
+#include "ia32_private.h"
+#include "ia32.h"
+
+static volatile unsigned *apic_base = NULL;
+
+/*
+ * NOT PUBLIC
+ */
+void init_apic(void)
+{
+	unsigned buffer[2];
+
+	/*
+	 * Read IA32_APIC_BASE
+	 */
+	read_msr(buffer, 0x1B);
+
+	/*
+	 * If not enabled, set to enabled
+	 */
+	buffer[0] |= (1UL << 11);
+	write_msr(buffer, 0x1B);
+
+	/*
+	 * Store the memory address of the APIC
+	 */
+	apic_base = (unsigned *)(buffer[0] & (0xFFFFFUL << 12));
+}
+
+BOOL is_apic_supported()
+{
+	unsigned buffer[2];
+
+	/*
+	 * Read IA32_APIC_BASE
+	 */
+	read_msr(buffer, 0x1B);
+
+	return (buffer[0] & (1UL << 11)) ? TRUE : FALSE;
+}
+
+BOOL apic_configure(char oneshot, char enableint, char vector, char divisor)
+{
+	unsigned temp = 0;
+
+	if (!apic_base)
+		return (FALSE);
+
+	temp = (unsigned)vector;
+	if (!enableint)
+		temp |= (1UL << 16);
+	if (!oneshot)
+		temp |= (1UL << 17);
+
+	apic_base[0x320 / sizeof(*apic_base)] = temp;
+
+	temp = 0;
+	temp |= divisor;
+	if (temp & ~APIC_DIV_1)
+		return FALSE;
+
+	apic_base[0x3E0 / sizeof(*apic_base)] = temp;
+
+	return (TRUE);
+}
+
+BOOL apic_write_counter(unsigned count)
+{
+	if (!apic_base)
+		return (FALSE);
+
+	apic_base[0x380 / sizeof(*apic_base)] = count;
+
+	return (TRUE);
+}
+
+unsigned apic_read_counter()
+{
+	if (!apic_base)
+		return (0);
+
+	return apic_base[0x390 / sizeof(*apic_base)];
+}
---
+++ platforms/ia32/apic.h
@@ -0,0 +1,50 @@
+/*
+ * DiegOS Operating System source code
+ *
+ * Copyright (C) 2012 - 2022 Diego Gallizioli
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#ifndef _APIC_H_
+#define _APIC_H_
+
+enum {
+	/* 0000b */
+	APIC_DIV_2,
+	/* 0001b */
+	APIC_DIV_4,
+	/* 0010b */
+	APIC_DIV_8,
+	/* 0011b */
+	APIC_DIV_16,
+	/* 1000b */
+	APIC_DIV_32 = 8,
+	/* 1001b */
+	APIC_DIV_64,
+	/* 1010b */
+	APIC_DIV_128,
+	/* 1011b */
+	APIC_DIV_1
+};
+
+BOOL is_apic_supported(void);
+
+BOOL apic_configure(char oneshot, char enableint, char vector, char divisor);
+
+BOOL apic_write_counter(unsigned count);
+
+unsigned apic_read_counter(void);
+
+#endif
