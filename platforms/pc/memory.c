977f017a8a863afcbe3216506323193eaa1e6c3c
Diego <diego.gallizioli@libero.it>
2022-09-22 00:10:16 +0200

    platforms/pc/memory.c: implement platform dependent memory functions
    Placeholders right now

---
+++ platforms/pc/memory.c
@@ -0,0 +1,32 @@
+/*
+ * DiegOS Operating System source code
+ *
+ * Copyright (C) 2012 - 2022 Diego Gallizioli
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#include "../../kernel/platform_include.h"
+
+void cacheable_memory(void **base, unsigned long *size)
+{
+	*base = (void *)(0x100000UL);
+	*size = 0x700000UL;
+}
+
+void io_memory(void **base, unsigned long *size)
+{
+	*base = (void *)0;
+	*size = 0;
+}
