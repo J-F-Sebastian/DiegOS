4a81c9a2ab917ec4c2798440727d83fe5d4264f1
Diego <diego.gallizioli@libero.it>
2022-11-14 23:36:29 +0100

    CX82310: new platform
    All SoC registers are described in the header files

---
+++ platforms/ARM/CX82310/cx82310_utils.h
@@ -0,0 +1,32 @@
+/*
+ * DiegOS Operating System source code
+ *
+ * Copyright (C) 2012 - 2022 Diego Gallizioli
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#ifndef _CX82310_UTILS_H_
+#define _CX82310_UTILS_H_
+
+/*
+ * All registers are 32 bit wide
+ * Indexes are 0 based
+ * Indexes address registers
+ */
+
+#define CX82310_REG(base_addr, reg) (base_addr + reg*4)
+
+#endif
+
