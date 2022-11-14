32491e84d3ef18d635cd8fd92b03d67206aeafda
Diego <diego.gallizioli@libero.it>
2022-11-14 23:36:29 +0100

    CX82310: new platform
    All SoC registers are described in the header files

---
+++ platforms/ARM/CX82310/cx82310_clk.h
@@ -0,0 +1,34 @@
+/*
+ * DiegOS Operating System source code
+ *
+ * Copyright (C) 2012 - 2015 Diego Gallizioli
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#ifndef _CX82310_CLK_H_
+#define _CX82310_CLK_H_
+
+/*
+ * All registers are 32 bit wide
+ */
+
+#define CX82310CLK_BASE 0x350068UL
+
+enum CX82310CLK {
+	FCLK_CTL,
+	BCLK_CTL
+};
+
+#endif
