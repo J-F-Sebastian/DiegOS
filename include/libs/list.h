e9fba2c11e0d9a7c93a29f679e63d7af6ce0b7e8
Diego <diego.gallizioli@libero.it>
2022-11-17 21:23:45 +0100

    include/libs/list.h: add list_node_next and list_node_prev

--- include/libs/list.h
+++ include/libs/list.h
@@ -22,6 +22,11 @@
 
 #include "list_type.h"
 
+/*
+ * Static initializer for global lists
+ * to be available prior to calling any
+ * function
+ */
 #define LIST_STATIC_INIT(listname) \
 list_inst listname = {            \
     .head = NULL,                   \
@@ -29,14 +34,71 @@ list_inst listname = {            \
     .counter = 0                    \
 }
 
+/*
+ * Same initialization as LIST_STATIC_INIT, but dynamic.
+ * The list instance must be allocated, no memory
+ * management is performed by the function.
+ *
+ * PARAMS IN
+ * list_inst *list - the list instance to be initialized
+ *
+ * RETURNS
+ * EOK    in case of success
+ * EINVAL if inst is NULL
+ */
 int list_init(list_inst * list);
 
+/*
+ * Add a node to the list by linking it to prev in different ways:
+ * if prev is NULL the data node is inserted at the head of the list,
+ * if prev is the last node in list the data node is inserted at the tail
+ * if the list, otherwise it is inserted between prev and the prev successor.
+ *
+ * PARAMS IN
+ * list_inst *list - the list instance
+ * list_node *prev - the node predecessor, i.e. the node that will be the prev of data.
+ * list_node *data - the node to be linked into the list
+ *
+ * RETURNS
+ * EOK    in case of success
+ * EINVAL in case list or data are NULL
+ */
 int list_add(list_inst * list, list_node * prev, list_node * data);
 
+/*
+ * Remove (unlink) a node from a list; no memory management is performed on data.
+ *
+ * PARAMS IN
+ * list_inst *list - the list instance
+ * list_node *data - the node to be unlinked
+ *
+ * RETURNS 
+ * EOK    in case of success
+ * EINVAL if list or data are NULL
+ */ 
 int list_remove(list_inst * list, list_node * data);
 
+/*
+ * Search for specific data by calling fn() function for each node in the list;
+ * when the function return value is TRUE the search stops and the node selected
+ * for processing is returned.
+ * If fn() never returns TRUE, then the search failed and NULL is returned.
+ *
+ * PARAMS IN
+ * list_inst *list - the list instance
+ * void *param     - search data, to be used by fn()
+ * BOOL(*fn)(list_node *, void *) - function to compare each node with data in param and
+ *                                  evaluate if they are equal or not; if they are equal
+ *                                  return TRUE.
+ *
+ * RETURNS
+ * NULL in case no node could be found, or a pointer to a node inside the list matching param.
+ */
 list_node *list_search(list_inst * list, void *param, BOOL(*fn) (list_node *, void *));
 
+/*
+ * Utility functions, used to avoid typecasts and unaligned accesses
+ */
 inline void *list_head(list_inst * list)
 {
 	return ((list) ? (list->head) : (NULL));
@@ -50,6 +112,16 @@ inline void *list_tail(list_inst * list)
 inline uint32_t list_count(list_inst * list)
 {
 	return ((list) ? (list->counter) : (0));
+}
+
+inline void *list_node_next(list_node *node)
+{
+	return (void *)(node->next);
+}
+
+inline void *list_node_prev(list_node *node)
+{
+	return (void *)(node->prev);
 }
 
 #endif				// LIST_H_INCLUDED
