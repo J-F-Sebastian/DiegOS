61c4a30ca1960323a2521b826ef618b98734a3e3
J-F-Sebastian <J-F-Sebastian@users.noreply.github.com>
2025-05-05 18:33:55 +0200

    libs/libc/ansi/mktime.c: refactor code and fix overflow handling

--- libs/libc/ansi/mktime.c
+++ libs/libc/ansi/mktime.c
@@ -19,124 +19,151 @@
 
 #include	<time.h>
 #include	<limits.h>
+#include	<errno.h>
 
 #include	"loc_time.h"
 
 time_t mktime(struct tm *timeptr)
 {
-	long day, year;
+	long day;
 	int tm_year;
 	int yday, month;
 	long seconds;
-	int overflow;
 	long int dst;
+	struct tm copy;
 
-	timeptr->tm_min += timeptr->tm_sec / 60;
-	timeptr->tm_sec %= 60;
-	if (timeptr->tm_sec < 0) {
-		timeptr->tm_sec += 60;
-		timeptr->tm_min--;
+	if (!timeptr)
+		return (time_t) (-1);
+
+	if (timeptr->tm_year < (EPOCH_YR - YEAR0))
+		return (time_t) (-1);
+
+	copy = *timeptr;
+
+	copy.tm_min += copy.tm_sec / 60;
+	copy.tm_sec %= 60;
+	if (copy.tm_sec < 0) {
+		copy.tm_sec += 60;
+		copy.tm_min--;
 	}
-	timeptr->tm_hour += timeptr->tm_min / 60;
-	timeptr->tm_min = timeptr->tm_min % 60;
-	if (timeptr->tm_min < 0) {
-		timeptr->tm_min += 60;
-		timeptr->tm_hour--;
+	copy.tm_hour += copy.tm_min / 60;
+	copy.tm_min = copy.tm_min % 60;
+	if (copy.tm_min < 0) {
+		copy.tm_min += 60;
+		copy.tm_hour--;
 	}
-	day = timeptr->tm_hour / 24;
-	timeptr->tm_hour = timeptr->tm_hour % 24;
-	if (timeptr->tm_hour < 0) {
-		timeptr->tm_hour += 24;
+	day = copy.tm_hour / 24;
+	copy.tm_hour = copy.tm_hour % 24;
+	if (copy.tm_hour < 0) {
+		copy.tm_hour += 24;
 		day--;
 	}
-	timeptr->tm_year += timeptr->tm_mon / 12;
-	timeptr->tm_mon %= 12;
-	if (timeptr->tm_mon < 0) {
-		timeptr->tm_mon += 12;
-		timeptr->tm_year--;
+	copy.tm_year += copy.tm_mon / 12;
+	copy.tm_mon %= 12;
+	if (copy.tm_mon < 0) {
+		copy.tm_mon += 12;
+		copy.tm_year--;
 	}
-	day += (timeptr->tm_mday - 1);
+	day += (copy.tm_mday - 1);
 	while (day < 0) {
-		if (--timeptr->tm_mon < 0) {
-			timeptr->tm_year--;
-			timeptr->tm_mon = 11;
+		if (--copy.tm_mon < 0) {
+			copy.tm_year--;
+			copy.tm_mon = 11;
 		}
-		day += days_per_month[LEAPYEAR(YEAR0 + timeptr->tm_year)][timeptr->tm_mon];
+		day += days_per_month[LEAPYEAR(YEAR0 + copy.tm_year)][copy.tm_mon];
 	}
-	while (day >= days_per_month[LEAPYEAR(YEAR0 + timeptr->tm_year)][timeptr->tm_mon]) {
-		day -= days_per_month[LEAPYEAR(YEAR0 + timeptr->tm_year)][timeptr->tm_mon];
-		if (++(timeptr->tm_mon) == 12) {
-			timeptr->tm_mon = 0;
-			timeptr->tm_year++;
+	while (day >= days_per_month[LEAPYEAR(YEAR0 + copy.tm_year)][copy.tm_mon]) {
+		day -= days_per_month[LEAPYEAR(YEAR0 + copy.tm_year)][copy.tm_mon];
+		if (++(copy.tm_mon) == 12) {
+			copy.tm_mon = 0;
+			copy.tm_year++;
 		}
 	}
-	timeptr->tm_mday = day + 1;
+	copy.tm_mday = day + 1;
 	tzset();
-	year = EPOCH_YR;
-	if (timeptr->tm_year < year - YEAR0)
-		return (time_t) - 1;
 	seconds = 0;
 	day = 0;		/* means days since day 0 now */
-	overflow = 0;
 
-	/* Assume that when day becomes negative, there will certainly
+	/*
+	 * Assume that when day becomes negative, there will certainly
 	 * be overflow on seconds.
 	 * The check for overflow needs not to be done for leapyears
 	 * divisible by 400.
-	 * The code only works when year (1970) is not a leapyear.
+	 * The code only works when year (2015) is not a leapyear.
 	 */
 #if LEAPYEAR(EPOCH_YR)
 #error	EPOCH_YR is a Leap Year !!!
 #endif
-	tm_year = timeptr->tm_year + YEAR0;
+	tm_year = copy.tm_year + YEAR0;
 
-	if (LONG_MAX / 365 < tm_year - year)
-		overflow++;
-	day = (tm_year - year) * 365;
-	if (LONG_MAX - day < (tm_year - year) / 4 + 1)
-		overflow++;
-	day += (tm_year - year) / 4 + ((tm_year % 4) && tm_year % 4 < year % 4);
-	day -= (tm_year - year) / 100 + ((tm_year % 100) && tm_year % 100 < year % 100);
-	day += (tm_year - year) / 400 + ((tm_year % 400) && tm_year % 400 < year % 400);
+	/* OVERFLOW */
+	if (LONG_MAX / 365 < tm_year - EPOCH_YR)
+		goto overflow;
+
+	day = (tm_year - EPOCH_YR) * 365;
+	/* OVERFLOW */
+	if (LONG_MAX - day < (tm_year - EPOCH_YR) / 4 + 1)
+		goto overflow;
+
+	day += (tm_year - EPOCH_YR) / 4 + ((tm_year % 4) && tm_year % 4 < EPOCH_YR % 4);
+	day -= (tm_year - EPOCH_YR) / 100 + ((tm_year % 100) && tm_year % 100 < EPOCH_YR % 100);
+	day += (tm_year - EPOCH_YR) / 400 + ((tm_year % 400) && tm_year % 400 < EPOCH_YR % 400);
 
 	yday = month = 0;
-	while (month < timeptr->tm_mon) {
+	while (month < copy.tm_mon) {
 		yday += days_per_month[LEAPYEAR(tm_year)][month];
 		month++;
 	}
-	yday += (timeptr->tm_mday - 1);
+	yday += (copy.tm_mday - 1);
+	/* OVERFLOW */
 	if (day + yday < 0)
-		overflow++;
+		goto overflow;
+
 	day += yday;
 
-	timeptr->tm_yday = yday;
-	timeptr->tm_wday = (day + 4) % 7;	/* day 0 was thursday (4) */
+	copy.tm_yday = yday;
+	copy.tm_wday = (day + 4) % 7;	/* day 0 was thursday (4) */
 
-	seconds = ((timeptr->tm_hour * 60L) + timeptr->tm_min) * 60L + timeptr->tm_sec;
+	seconds = ((copy.tm_hour * 60L) + copy.tm_min) * 60L + copy.tm_sec;
 
+	/* OVERFLOW */
 	if ((LONG_MAX - seconds) / SECS_DAY < day)
-		overflow++;
+		goto overflow;
+
 	seconds += day * SECS_DAY;
 
 	/* Now adjust according to timezone and daylight saving time */
 
+	/* OVERFLOW */
 	if (((timezone > 0) && (LONG_MAX - timezone < seconds))
 	    || ((timezone < 0) && (seconds < -timezone)))
-		overflow++;
+		goto overflow;
+
 	seconds += timezone;
 
-	if (timeptr->tm_isdst) {
+	if (copy.tm_isdst > 0) {
 		dst = dst_off;
-	} else {
+	} else if (copy.tm_isdst == 0) {
 		dst = 0;
+	} else {
+		dst = (daylight) ? dst_off : 0;
+		copy.tm_isdst = daylight;
 	}
 
+	/* dst is always non-negative */
+	/* OVERFLOW */
 	if (dst > seconds)
-		overflow++;	/* dst is always non-negative */
+		goto overflow;
+
 	seconds -= dst;
 
-	if (overflow)
-		return (time_t) - 1;
+	/*
+	 * We are fine now. Copy local computed values back to timeptr.
+	 */
+	*timeptr = copy;
+	return (time_t) (seconds);
 
-	return (time_t) seconds;
+ overflow:
+	errno = EOVERFLOW;
+	return (time_t) (-1);
 }
