803f38e2d301fd2bb3260e3013807b9e6a26a6ae
J-F-Sebastian <J-F-Sebastian@users.noreply.github.com>
2025-05-06 23:52:50 +0200

    libs/libc/ansi/localtime.c: use convtime

--- libs/libc/ansi/localtime.c
+++ libs/libc/ansi/localtime.c
@@ -22,55 +22,12 @@
 
 #include "loc_time.h"
 
-/*
- * Offsets for Gauss's algorithm.
- * First row is for common years.
- * Second row for leap years.
- */
-static const int month_offset[2][12] = {
-	{0, 3, 3, 6, 1, 4, 6, 2, 5, 0, 3, 5},
-	{0, 3, 4, 0, 2, 5, 0, 3, 6, 1, 4, 6}
-};
-
-/* Calculate day of week with Gauss's Algorithm. Sunday == 0. */
-static int wday(int year, int month, int day)
-{
-	int ofs = month_offset[LEAPYEAR(year)][month];
-	year -= 1;
-
-	return ((day + ofs + 5 * (year % 4) + 4 * (year % 100) + 6 * (year % 400)) % 7);
-}
-
-/*
- * Not needed anymore, see EPOCH_SECS
- * Keep the code for reference.
- */
-#if 0
-static time_t adjust_to_epoch()
-{
-	time_t retval = 0;
-	int secy, myvar = YEAR0;
-
-	while (myvar < EPOCH_YR) {
-		secy = YEARSIZE(myvar);
-		secy *= SECS_DAY;
-		retval += secy;
-		myvar++;
-	}
-
-	return (retval);
-}
-#endif
-
 struct tm *localtime(const time_t * timer)
 {
 	time_t adj = EPOCH_SECS;
-	static struct tm retval = { 0 };
-	time_t counter;
-	int myvar;
-	int secy = 0;
+	struct tm *retval = NULL;
 
-	if (!timer || (*timer < 0)) {
+	if (!timer) {
 		errno = EINVAL;
 		return (NULL);
 	}
@@ -83,52 +40,13 @@ struct tm *localtime(const time_t * timer)
 		tmp -= dst_off;
 	}
 
-	retval.tm_sec = tmp % 60;	/* seconds after the minute [0, 59] */
-	retval.tm_min = (tmp % (SECS_HOUR)) / 60;	/* minutes after the hour [0, 59] */
-	retval.tm_hour = (tmp % (SECS_DAY)) / (60 * 60);	/* hours since midnight [0, 23] */
-
-	/* count seconds per year up to tmp, keep track of passing years */
-	counter = 0;
-	myvar = 0;
-	while (counter < tmp) {
-		secy = YEARSIZE(YEAR0 + myvar);
-		secy *= SECS_DAY;
-		counter += secy;
-		myvar++;
-	}
-	retval.tm_year = (myvar) ? (myvar - 1) : myvar;	/* years since 2000 */
-
-	/* compute remaining seconds in tmp */
-	if (counter != tmp) {
-		counter -= secy;
+	if (tmp < 0) {
+		errno = EOVERFLOW;
+		return (NULL);
 	}
 
-	tmp -= counter;
-	/* compute remaining days */
-	retval.tm_yday = tmp / (SECS_DAY);	/* days since January 1 [0, 365] */
+	retval = convtime(&tmp);
+	retval->tm_isdst = daylight;	/* Daylight Saving Time flag */
 
-	counter = 0;
-	myvar = 0;
-	while (counter < tmp) {
-		secy = days_per_month[LEAPYEAR(YEAR0 + retval.tm_year)][myvar];
-		secy *= SECS_DAY;
-		counter += secy;
-		if (++myvar == 12)
-			myvar = 0;
-	}
-	if (counter != tmp) {
-		myvar--;
-		counter -= secy;
-	}
-	retval.tm_mon = myvar;	/* months since January [0, 11] */
-
-	/* compute remaining seconds in tmp */
-	tmp -= counter;
-	retval.tm_mday = (tmp / (SECS_DAY)) + 1;	/* day of the month [1, 31] */
-
-	retval.tm_wday = wday(YEAR0 + retval.tm_year, retval.tm_mon, retval.tm_mday);	/* days since Sunday [0, 6] */
-
-	retval.tm_isdst = daylight;	/* Daylight Saving Time flag */
-
-	return (&retval);
+	return (retval);
 }
