4527ee16058a8159e367f0c20b537f2e1a11d54a
J-F-Sebastian <J-F-Sebastian@users.noreply.github.com>
2025-12-14 16:34:28 +0100

    build/pc: run qemu as user

--- build/pc/qemu_ns.sh
+++ build/pc/qemu_ns.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 #sudo qemu-system-i386 -cpu pentium2 -m 16 -netdev tap,id=tap0 -device rtl8139,netdev=tap0 -vga std -fda bootflop.img -boot a -serial telnet:127.0.0.1:10000,server,nowait -monitor stdio
-sudo qemu-system-i386 -cpu pentium2 -m 16 -vga std -fda bootimage.img -boot a -serial telnet:127.0.0.1:10000,server,nowait -monitor stdio
+qemu-system-i386 -cpu pentium2 -m 16 -vga std -fda bootimage.img -boot a -serial telnet:127.0.0.1:10000,server,nowait -monitor stdio
