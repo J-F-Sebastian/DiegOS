23a0f11a5ce68f4301eb72d475aa7a49c5ec3f6b
J-F-Sebastian <J-F-Sebastian@users.noreply.github.com>
2025-12-18 22:03:53 +0100

    boot/pc/bootmbr32.s: indentation

--- boot/pc/bootmbr32.s
+++ boot/pc/bootmbr32.s
@@ -147,9 +147,9 @@ DIEGOS_START    EQU 0x00110000
 DIEGOS_LOAD     EQU 0x00010000
 
 ; Descriptors at beginning of RAM
-IDT_DESC	EQU RAM_START
+IDT_DESC        EQU RAM_START
 ; IDT size is 8 bytes x 256 interrupts = 2048 bytes, 0x800
-GDT_DESC	EQU IDT_DESC + 0x0800
+GDT_DESC        EQU IDT_DESC + 0x0800
 ; GDT entries: 3 descriptors for NULL, CODE, DATA
 ; FLAT model, max priority
 ; GDT has room for a total of 8 descriptors
@@ -165,42 +165,41 @@ LINEAR_PROTO_CODE_HI 	EQU 0x00C09B00
 LINEAR_PROTO_DATA_HI 	EQU 0x00CF9200
 
 ; Protection Enable Bit in CR0, no paging
-PE_BIT EQU 1
-CD_BIT EQU 1<<30
-NW_BIT EQU 1<<29
+PE_BIT          EQU 1
+CD_BIT          EQU 1<<30
+NW_BIT          EQU 1<<29
 
 BootLoader:
 CLD
 ; Init DS,SS to have a temporary stack and data access
 ; Increment counters
-XOR	AX, AX
-MOV	DS, AX
-MOV	SS, AX
-MOV	SP, STACK16_START
-MOV	BP, SP
+XOR     AX, AX
+MOV     DS, AX
+MOV     SS, AX
+MOV     SP, STACK16_START
+MOV     BP, SP
 
-MOV	SI, HelloString
-CALL	PrintString
+MOV     SI, HelloString
+CALL    PrintString
 
 ; Parse the Master Boot Record
-MOV	BX, MBR
-MOV	CX, 4
+MOV     BX, MBR
+MOV     CX, 4
 MbrLoop:
-TEST	BYTE [BX], 0x80
-JNZ	MbrFound
-ADD	BX, 0x10
-LOOP	MbrLoop
+TEST    BYTE [BX], 0x80
+JNZ     MbrFound
+ADD     BX, 0x10
+LOOP    MbrLoop
 MbrFound:
-CMP	CX, 0
-JE	MbrError
+CMP     CX, 0
+JE      MbrError
 
 STI
 
 ;reset first disk drive
-MOV	AH, 0x00
-;MOV	DL, 0x00
-INT	0x13
-JC	Error
+MOV     AH, 0x00
+INT     0x13
+JC      Error
 
 ;CH = low eight bits of maximum cylinder number
 ;CL = maximum sector number (bits 5-0)
@@ -217,153 +216,152 @@ JC	Error
 
 ; set destination buffer
 ; NOTE: the buffer is 64KBytes limited !!!
-MOV    SI, WORD [BX + 12]
-MOV    [STACK16_START], SI
-MOV    AX, WORD DIEGOS_LOAD>>4
-MOV    ES, AX
-MOV    BP, AX
+MOV     SI, WORD [BX + 12]
+MOV     [STACK16_START], SI
+MOV     AX, WORD DIEGOS_LOAD>>4
+MOV     ES, AX
+MOV     BP, AX
 
 ;1.44 MB Disk Format support 18 sectors per track,
 ; or 0x12. the first sector is used by boot code.
 ; Function 0x02 of INT 0x13, read data from disk
-;AH	function 0x02
-;AL	sectors to be read, first round not in loop
+;AH = function 0x02
+;AL = sectors to be read, first round not in loop
 MOV     AX, 0x0201
-;CH	starting cylinder/track
-;CL	starting sector
+;CH = starting cylinder/track
+;CL = starting sector
 MOV     CX, WORD [BX + 2]
-;DH	starting head
-;DL	1st disk unit
-MOV	DH, BYTE [BX + 1]
-XOR	BX, BX
+;DH = starting head
+;DL = 1st disk unit
+MOV     DH, BYTE [BX + 1]
+XOR     BX, BX
 
 readOneSector:
 
-INT	0x13
-JC	Error
+INT     0x13
+JC      Error
 
-DEC 	SI
-JZ  	StartDiegOS
+DEC     SI
+JZ      StartDiegOS
 
-ADD 	BX, BYTESPERSEC
-JNC 	NoNewSegment
+ADD     BX, BYTESPERSEC
+JNC     NoNewSegment
 
-ADD 	BP, WORD DIEGOS_LOAD>>4
-MOV 	ES, BP
+ADD     BP, WORD DIEGOS_LOAD>>4
+MOV     ES, BP
 NoNewSegment:
 
-MOV	AX, 0x0201
-INC 	CL
-CMP 	CL, 0x12
-JNA	readOneSector
+MOV     AX, 0x0201
+INC     CL
+CMP     CL, 0x12
+JNA     readOneSector
 
-MOV 	CL, 1
-ADD 	CH, DH
-XOR	DH, 0x01
+MOV     CL, 1
+ADD     CH, DH
+XOR     DH, 0x01
 
-JMP readOneSector
+JMP     readOneSector
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
 StartDiegOS:
-MOV	SI, StartString
-CALL	PrintString
-CALL	StopMotor
+MOV     SI, StartString
+CALL    PrintString
+CALL    StopMotor
 ;Clear interrupts
 CLI
 
 ;Enable Gate A20
-MOV	AX, 0x2401
-INT	0x15
-JNC	A20Enabled
+MOV     AX, 0x2401
+INT     0x15
+JNC     A20Enabled
 
-CALL	A20Enable
+CALL    A20Enable
 
 A20Enabled:
 ;Assign segments for proper kernel start
-XOR	AX, AX
-MOV	ES, AX
+XOR     AX, AX
+MOV     ES, AX
 
 ; IDT is initialized by kernel
 ; Setup GDT
 ; First GDT entry is NULL
-MOV 	DWORD [GDT_DESC], 0
-MOV 	DWORD [GDT_DESC + 4], 0
+MOV     DWORD [GDT_DESC], 0
+MOV     DWORD [GDT_DESC + 4], 0
 
 ; Second GDT entry is for FLAT MEMORY CODE
-MOV 	DWORD [GDT_DESC + 8], LINEAR_PROTO_CODE_LO
-MOV 	DWORD [GDT_DESC + 12], LINEAR_PROTO_CODE_HI
+MOV     DWORD [GDT_DESC + 8], LINEAR_PROTO_CODE_LO
+MOV     DWORD [GDT_DESC + 12], LINEAR_PROTO_CODE_HI
 
 ; Third GDT entry is for FLAT MEMORY DATA
-MOV 	DWORD [GDT_DESC + 16], DWORD LINEAR_PROTO_DATA_LO
-MOV 	DWORD [GDT_DESC + 20], DWORD LINEAR_PROTO_DATA_HI
+MOV     DWORD [GDT_DESC + 16], DWORD LINEAR_PROTO_DATA_LO
+MOV     DWORD [GDT_DESC + 20], DWORD LINEAR_PROTO_DATA_HI
 
 ; Setup IDTR
-MOV	WORD  [IDTR_START], (256 * 8) - 1
-MOV 	DWORD [IDTR_START + 2], DWORD IDT_DESC
-
+MOV     WORD  [IDTR_START], (256 * 8) - 1
+MOV     DWORD [IDTR_START + 2], DWORD IDT_DESC
 
 ; Setup GDTR
-MOV	WORD  [GDTR_START], (8 * 8) - 1
-MOV 	DWORD [GDTR_START + 2], DWORD GDT_DESC
+MOV     WORD  [GDTR_START], (8 * 8) - 1
+MOV     DWORD [GDTR_START + 2], DWORD GDT_DESC
 
 ; execute a 32 bit LGDT
-o32	LGDT [GDTR_START]
+o32     LGDT [GDTR_START]
 
 ; enter protected mode
-MOV 	EBX, CR0
-OR	    EBX, PE_BIT
+MOV     EBX, CR0
+OR      EBX, PE_BIT
 AND     EBX, ~(CD_BIT | NW_BIT)
-MOV 	CR0, EBX
+MOV     CR0, EBX
 
 ; clear prefetch queue, sets CS
-JMP 	DWORD LINEAR_CODE_SEL:CLEAR_LABEL
+JMP     DWORD LINEAR_CODE_SEL:CLEAR_LABEL
 
 [BITS 32]
 
 CLEAR_LABEL:
 ; make All segs address 4G of linear memory
-MOV	ECX, DWORD LINEAR_DATA_SEL
-MOV	DS, CX
-MOV 	ES, CX
-MOV	FS, CX
-MOV	GS, CX
+MOV     ECX, DWORD LINEAR_DATA_SEL
+MOV     DS, CX
+MOV     ES, CX
+MOV     FS, CX
+MOV     GS, CX
 
 ;init stack
-MOV	EDI, DWORD STACK32_START
-MOV	SS,  CX
-MOV	ESP, EDI
-MOV	EBP, EDI
+MOV     EDI, DWORD STACK32_START
+MOV     SS,  CX
+MOV     ESP, EDI
+MOV     EBP, EDI
 
 ; execute a 32 bit LIDT
-LIDT 	[IDTR_START]
+LIDT    [IDTR_START]
 MOV     ESI, DWORD DIEGOS_LOAD
 MOV     EDI, DWORD DIEGOS_START
 MOV     CX, WORD [STACK16_START]
 SHL     ECX, 7
 REP     MOVSD
 ; START THE SYSTEM!
-JMP	DIEGOS_START
+JMP     DIEGOS_START
 
 [BITS 16]
 
 MbrError:
-MOV	SI, MbrString
-JMP	StopError
+MOV     SI, MbrString
+JMP     StopError
 Error:
-MOV	SI, ErrorString
+MOV     SI, ErrorString
 
 StopError:
-CALL	PrintString
-CALL	StopMotor
+CALL    PrintString
+CALL    StopMotor
 HLT
-JMP	$
+JMP     $
 
 StopMotor:
 ;stop disk motor
-MOV	AL, 0x00
-MOV	DX, 0x3F2
-OUT 	DX, AL
+MOV     AL, 0x00
+MOV     DX, 0x3F2
+OUT     DX, AL
 RET
 
 ; PrintString
@@ -372,40 +370,40 @@ RET
 ; AX
 ; BX
 PrintString:
-MOV	AH, 0x0E
-MOV	BX, 0x0007
+MOV     AH, 0x0E
+MOV     BX, 0x0007
 PrintNext:
 LODSB
-CMP	AL, 0x00
-JE	PrintQuit
-INT	0x10
-JMP	PrintNext
+CMP     AL, 0x00
+JE      PrintQuit
+INT     0x10
+JMP     PrintNext
 PrintQuit:
 RET
 
 A20Enable:
-MOV	AL, 0xD1
-OUT	0x64, AL
-call	empty_8042
-MOV	AL, 0xDF
-OUT	0x64, AL
-CALL	empty_8042
+MOV     AL, 0xD1
+OUT     0x64, AL
+CALL    empty_8042
+MOV     AL, 0xDF
+OUT     0x64, AL
+CALL    empty_8042
 RET
 
 empty_8042:
-MOV	CX, 0x100
-LOOP	$
-IN	AL, 0x64
-TEST	AL, 0x02
-JNZ	empty_8042
+MOV     CX, 0x100
+LOOP    $
+IN      AL, 0x64
+TEST    AL, 0x02
+JNZ     empty_8042
 RET
 
-HelloString 	db 0xDB,0xDB,0xDD,'DiegOS MBR Loader 3.0', 0x0D, 0x0A, 0x00
-MbrString 	db 'Partition not found', 0x00
-ErrorString	db 'Error', 0x00
+HelloString     db 0xDB,0xDB,0xDD,'DiegOS MBR Loader 3.0', 0x0D, 0x0A, 0x00
+MbrString       db 'Partition not found', 0x00
+ErrorString     db 'Error', 0x00
 StartString     db 0x0D, 0x0A, 'Starting..', 0x0D, 0x0A, 0x00
-TIMES	446 - ($ - $$) db 0xdf
+TIMES 446 - ($ - $$) db 0xdf
 
 MBR:
 TIMES 64 DB 0
-DW	0xAA55
+DW      0xAA55
