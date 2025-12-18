d70cd2ba133e98d04f041a35cf68337b2bb4f5ce
J-F-Sebastian <J-F-Sebastian@users.noreply.github.com>
2025-12-18 23:15:42 +0100

    boot/pc/bootsector32.s: indentation

--- boot/pc/bootsector32.s
+++ boot/pc/bootsector32.s
@@ -135,35 +135,35 @@
 [ORG 0x7C00]
 [BITS 16]
 
-BYTESPERSEC	EQU 512
-RAM_START	EQU 0x0600
+BYTESPERSEC     EQU 512
+RAM_START       EQU 0x0600
 STACK16_START	EQU 0x7000
-STACK32_START	EQU 0x00010000
-DIEGOS_START	EQU 0x00110000
+STACK32_START   EQU 0x00010000
+DIEGOS_START    EQU 0x00110000
 DIEGOS_LOAD     EQU 0x00010000
 
 ; Descriptors at beginning of RAM
-IDT_DESC	EQU RAM_START
+IDT_DESC        EQU RAM_START
 ; IDT size is 8 bytes x 256 interrupts = 2048 bytes, 0x800
-GDT_DESC	EQU IDT_DESC + 0x0800
+GDT_DESC        EQU IDT_DESC + 0x0800
 ; GDT entries: 3 descriptors for NULL, CODE, DATA
 ; FLAT model, max priority
 ; GDT has room for a total of 8 descriptors
-IDTR_START	EQU GDT_DESC + 0x40 + 2
-GDTR_START	EQU IDTR_START + 8
+IDTR_START      EQU GDT_DESC + 0x40 + 2
+GDTR_START      EQU IDTR_START + 8
 
 ; equates for building GDT
-LINEAR_CODE_SEL		EQU 1*8
-LINEAR_DATA_SEL		EQU 2*8
-LINEAR_PROTO_CODE_LO	EQU 0x000000A0
-LINEAR_PROTO_DATA_LO 	EQU 0x0000FFFF
-LINEAR_PROTO_CODE_HI 	EQU 0x00C09B00
-LINEAR_PROTO_DATA_HI 	EQU 0x00CF9200
+LINEAR_CODE_SEL         EQU 1*8
+LINEAR_DATA_SEL         EQU 2*8
+LINEAR_PROTO_CODE_LO    EQU 0x000000A0
+LINEAR_PROTO_DATA_LO    EQU 0x0000FFFF
+LINEAR_PROTO_CODE_HI    EQU 0x00C09B00
+LINEAR_PROTO_DATA_HI    EQU 0x00CF9200
 
 ; Protection Enable Bit in CR0, no paging
-PE_BIT EQU 1
-CD_BIT EQU 1<<30
-NW_BIT EQU 1<<29
+PE_BIT                  EQU 1
+CD_BIT                  EQU 1<<30
+NW_BIT                  EQU 1<<29
 
 ; BPB - BIOS Parameter Block
 ; Leave void, other tools must fill in proper data.
@@ -174,22 +174,21 @@ BootLoader:
 CLD
 ; Init DS,SS to have a temporary stack and data access
 ; Increment counters
-XOR	AX, AX
-MOV	DS, AX
-MOV	SS, AX
-MOV	SP, STACK16_START
-MOV	BP, SP
+XOR     AX, AX
+MOV     DS, AX
+MOV     SS, AX
+MOV     SP, STACK16_START
+MOV     BP, SP
 
-MOV	SI, HelloString
-CALL	PrintString
+MOV     SI, HelloString
+CALL    PrintString
 
 STI
 
 ;reset first disk drive
-MOV	AH, 0x00
-;MOV	DL, 0x00
-INT	0x13
-JC	Error
+MOV     AH, 0x00
+INT     0x13
+JC      Error
 
 ;CH = low eight bits of maximum cylinder number
 ;CL = maximum sector number (bits 5-0)
@@ -206,71 +205,71 @@ JC	Error
 
 ; set destination buffer
 ; NOTE: the buffer is 64KBytes limited !!!
-MOV 	SI, WORD [BPB + 0xE]
-DEC	SI
-MOV	AX, WORD DIEGOS_LOAD>>4
-MOV	ES, AX
-MOV 	BP, AX
+MOV     SI, WORD [BPB + 0xE]
+DEC     SI
+MOV     AX, WORD DIEGOS_LOAD>>4
+MOV     ES, AX
+MOV     BP, AX
 
 ;1.44 MB Disk Format support 18 sectors per track,
 ; or 0x12. the first sector is used by boot code.
 ; Function 0x02 of INT 0x13, read data from disk
-;AH	function 0x02
-;AL	sectors to be read, first round not in loop
+;AH = function 0x02
+;AL = sectors to be read, first round not in loop
 MOV     AX, 0x0201
-;CH	starting cylinder/track
-;CL	starting sector
+;CH = starting cylinder/track
+;CL = starting sector
 MOV     CX, 0x0002
-;DH	starting head
-;DL	1st disk unit
-MOV	DH, 0x00
-XOR	BX, BX
+;DH = starting head
+;DL = 1st disk unit
+MOV     DH, 0x00
+XOR     BX, BX
 
 readOneSector:
 
-INT	0x13
-JC	Error
+INT     0x13
+JC      Error
 
-SUB 	SI, 1
-JZ  	StartDiegOS
+SUB     SI, 1
+JZ      StartDiegOS
 
-PUSH	BX
-CALL	PrintDot
-POP	BX
+PUSH    BX
+CALL    PrintDot
+POP     BX
 
-ADD 	BX, BYTESPERSEC
-JNC 	NoNewSegment
+ADD     BX, BYTESPERSEC
+JNC     NoNewSegment
 
-ADD 	BP, WORD DIEGOS_LOAD>>4
-MOV 	ES, BP
+ADD     BP, WORD DIEGOS_LOAD>>4
+MOV     ES, BP
 NoNewSegment:
 
-MOV	AX, 0x0201
-INC 	CL
-CMP 	CL, 0x12
-JNA	readOneSector
+MOV     AX, 0x0201
+INC     CL
+CMP     CL, 0x12
+JNA     readOneSector
 
-MOV 	CL, 1
-ADD 	CH, DH
-XOR	DH, 0x01
+MOV     CL, 1
+ADD     CH, DH
+XOR     DH, 0x01
 
-JMP readOneSector
+JMP     readOneSector
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
 StartDiegOS:
-MOV	SI, StartString
-CALL	PrintString
-CALL	StopMotor
+MOV     SI, StartString
+CALL    PrintString
+CALL    StopMotor
 ;Clear interrupts
 CLI
 
 ;Enable Gate A20
-MOV	AX, 0x2401
-INT	0x15
-JNC	A20Enabled
+MOV     AX, 0x2401
+INT     0x15
+JNC     A20Enabled
 
-CALL	A20Enable
+CALL    A20Enable
 
 A20Enabled:
 ;Detect free memory
@@ -291,81 +290,81 @@ A20Enabled:
 ;MOV     DWORD [DIEGOS_VARS + 4], EAX
 
 ;Assign segments for proper kernel start
-XOR	AX, AX
-MOV	ES, AX
+XOR     AX, AX
+MOV     ES, AX
 
 ; IDT is initialized by kernel
 ; Setup GDT
 ; First GDT entry is NULL
-MOV 	DWORD [GDT_DESC], 0
-MOV 	DWORD [GDT_DESC + 4], 0
+MOV     DWORD [GDT_DESC], 0
+MOV     DWORD [GDT_DESC + 4], 0
 
 ; Second GDT entry is for FLAT MEMORY CODE
-MOV 	DWORD [GDT_DESC + 8], LINEAR_PROTO_CODE_LO
-MOV 	DWORD [GDT_DESC + 12], LINEAR_PROTO_CODE_HI
+MOV     DWORD [GDT_DESC + 8], LINEAR_PROTO_CODE_LO
+MOV     DWORD [GDT_DESC + 12], LINEAR_PROTO_CODE_HI
 
 ; Third GDT entry is for FLAT MEMORY DATA
-MOV 	DWORD [GDT_DESC + 16], DWORD LINEAR_PROTO_DATA_LO
-MOV 	DWORD [GDT_DESC + 20], DWORD LINEAR_PROTO_DATA_HI
+MOV     DWORD [GDT_DESC + 16], DWORD LINEAR_PROTO_DATA_LO
+MOV     DWORD [GDT_DESC + 20], DWORD LINEAR_PROTO_DATA_HI
 
 ; Setup IDTR
-MOV	WORD  [IDTR_START], (256 * 8) - 1
-MOV 	DWORD [IDTR_START + 2], DWORD IDT_DESC
+MOV     WORD  [IDTR_START], (256 * 8) - 1
+MOV     DWORD [IDTR_START + 2], DWORD IDT_DESC
 
 
 ; Setup GDTR
-MOV	WORD  [GDTR_START], (8 * 8) - 1
-MOV 	DWORD [GDTR_START + 2], DWORD GDT_DESC
+MOV     WORD  [GDTR_START], (8 * 8) - 1
+MOV     DWORD [GDTR_START + 2], DWORD GDT_DESC
 
 ; execute a 32 bit LGDT
-o32	LGDT [GDTR_START]
+o32     LGDT [GDTR_START]
 
 ; enter protected mode
-MOV 	EBX, CR0
-OR	EBX, PE_BIT
+MOV     EBX, CR0
+OR      EBX, PE_BIT
 AND     EBX, ~CD_BIT
-AND	EBX, ~NW_BIT
-MOV 	CR0, EBX
+AND     EBX, ~NW_BIT
+MOV     CR0, EBX
 
 ; clear prefetch queue, sets CS
-JMP 	DWORD LINEAR_CODE_SEL:CLEAR_LABEL
+JMP     DWORD LINEAR_CODE_SEL:CLEAR_LABEL
 
 [BITS 32]
 
 CLEAR_LABEL:
 ; make All segs address 4G of linear memory
-MOV	ECX, DWORD LINEAR_DATA_SEL
-MOV	DS, CX
-MOV 	ES, CX
-MOV	FS, CX
-MOV	GS, CX
+MOV     ECX, DWORD LINEAR_DATA_SEL
+MOV     DS, CX
+MOV     ES, CX
+MOV     FS, CX
+MOV     GS, CX
 
 ;init stack
-MOV	EDI, DWORD STACK32_START
-MOV	SS,  CX
-MOV	ESP, EDI
-MOV	EBP, EDI
+MOV     EDI, DWORD STACK32_START
+MOV     SS,  CX
+MOV     ESP, EDI
+MOV     EBP, EDI
 
 ; execute a 32 bit LIDT
-LIDT 	[IDTR_START]
-MOV ESI, DWORD DIEGOS_LOAD
-MOV EDI, DWORD DIEGOS_START
-XOR ECX, ECX
-MOV CX, WORD [BPB + 0xE]
-DEC CX
-SHL ECX, 7
-REP MOVSD
+LIDT    [IDTR_START]
+MOV     ESI, DWORD DIEGOS_LOAD
+MOV     EDI, DWORD DIEGOS_START
+XOR     ECX, ECX
+MOV     CX, WORD [BPB + 0xE]
+DEC     CX
+SHL     ECX, 7
+REP     MOVSD
 ; START THE SYSTEM!
-JMP	DIEGOS_START
+JMP     DIEGOS_START
 
 [BITS 16]
 
 Error:
-MOV	SI, ErrorString
-CALL	PrintString
-CALL	StopMotor
+MOV     SI, ErrorString
+CALL    PrintString
+CALL    StopMotor
 HLT
-JMP	$
+JMP     $
 
 ; StopMotor
 ; Stop the boot disk motor
@@ -374,9 +373,9 @@ JMP	$
 ; AL
 StopMotor:
 ;stop disk motor
-MOV	AL, 0x00
-MOV	DX, 0x3F2
-OUT 	DX, AL
+MOV     AL, 0x00
+MOV     DX, 0x3F2
+OUT     DX, AL
 RET
 
 ; PrintString
@@ -385,14 +384,14 @@ RET
 ; AX
 ; BX
 PrintString:
-MOV	AH, 0x0E
-MOV	BX, 0x0007
+MOV     AH, 0x0E
+MOV     BX, 0x0007
 PrintNext:
 LODSB
-CMP	AL, 0x00
-JE	PrintQuit
-INT	0x10
-JMP	PrintNext
+CMP     AL, 0x00
+JE      PrintQuit
+INT     0x10
+JMP     PrintNext
 PrintQuit:
 RET
 
@@ -403,31 +402,31 @@ RET
 ; BX
 PrintDot:
 ; Print a dot for every sector read in memory
-MOV	AX, 0x0EFE
-MOV	BX, 0x0007
-INT	0x10
+MOV     AX, 0x0EFE
+MOV     BX, 0x0007
+INT     0x10
 RET
 
 A20Enable:
-MOV	AL, 0xD1
-OUT	0x64, AL
-call	empty_8042
-MOV	AL, 0xDF
-OUT	0x64, AL
-CALL	empty_8042
+MOV     AL, 0xD1
+OUT     0x64, AL
+call    empty_8042
+MOV     AL, 0xDF
+OUT     0x64, AL
+CALL    empty_8042
 RET
 
 empty_8042:
-MOV	CX, 0x100
-LOOP	$
-IN	AL, 0x64
-TEST	AL, 0x02
-JNZ	empty_8042
+MOV     CX, 0x100
+LOOP    $
+IN      AL, 0x64
+TEST    AL, 0x02
+JNZ     empty_8042
 RET
 
-HelloString 	db 0xDB,0xDB,0xDD,'DiegOS Boot Loader 3.0', 0x0D, 0x0A, 0x00
-ErrorString	db 'Error', 0x00
+HelloString     db 0xDB,0xDB,0xDD,'DiegOS Boot Loader 3.0', 0x0D, 0x0A, 0x00
+ErrorString     db 'Error', 0x00
 StartString     db 0x0D, 0x0A, 'Starting ...', 0x0D, 0x0A, 0x00
 TIMES 510 - ($ - $$) db 0xdf
 
-DW	0xAA55
+DW      0xAA55
