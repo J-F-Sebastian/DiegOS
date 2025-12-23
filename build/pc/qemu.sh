4e95a85d72a3f0399636db68e1cc6be8f6f85d1f
J-F-Sebastian <J-F-Sebastian@users.noreply.github.com>
2025-12-23 18:05:25 +0100

    qemu.sh, qemu_ns.sh: use bootfloppy.img

--- build/pc/qemu.sh
+++ build/pc/qemu.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 #sudo qemu-system-i386 -cpu pentium2 -m 16 -netdev tap,id=tap0 -device rtl8139,netdev=tap0 -vga std -fda bootflop.img -boot a -serial telnet:127.0.0.1:10000,server,nowait -monitor stdio -S
-qemu-system-i386 -cpu pentium2 -m 32 -vga std -fda bootimage.img -boot a -serial telnet:127.0.0.1:10000,server,nowait -monitor stdio -S
+qemu-system-i386 -cpu pentium2 -m 32 -vga std -fda bootfloppy.img -boot a -serial telnet:127.0.0.1:10000,server,nowait -monitor stdio -S
 #sudo qemu-system-i386 -machine q35 -cpu pentium2 -m 16 -vga std -net nic,model=rtl8139 -fda bootflop.img -boot a -serial telnet:127.0.0.1:10000,server,nowait -monitor stdio -S
